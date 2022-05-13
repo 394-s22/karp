186a08c0e525dbf509c2b1e1bcee9f0a3ecf7f59
Bobo Tam <bobo513786356@gmail.com>
2022-05-12 21:42:10 -0500

    Grace W, Jaz, Bobo worked on decisionProblem

--- src/components/DecisionProblem.js
+++ src/components/DecisionProblem.js
@@ -1,3 +1,9 @@
+import React, { useReducer } from "react";
+import { Button, Icon, TextField, Paper, Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+// import "../styles/Form.css";
+
+
 const DecisionProblem = ({ output, setOutput }) => {
     
     const [decisionProblemInput, setDecisionProblemInput] = useReducer(
@@ -8,6 +14,73 @@ const DecisionProblem = ({ output, setOutput }) => {
           Arity: "",
           Certificate: "",
         }
+    );
+
+    const handleInput = (evt) => {
+      const name = evt.target.name;
+      const newValue = evt.target.value;
+      setFormInput({ [name]: newValue });
+    };
+    
+    return (
+      <div>
+        <div>
+          <TextField
+            label="Name"
+            name="Name"
+            multiline
+            minRows={4}
+            defaultValue={decisionProblemInput.name}
+            //className={classes.textField}
+            helperText="Enter Name"
+            onChange={handleInput}
+          />
+        </div>
+        <div>
+          <TextField
+            label="Instance"
+            name="Instance"
+            multiline
+            minRows={4}
+            defaultValue={decisionProblemInput.name}
+            //className={classes.textField}
+            helperText="Enter Instance"
+            onChange={handleInput}
+          />
+        </div>
+        <div>
+          <TextField
+            label="Arity"
+            name="Arity"
+            multiline
+            minRows={4}
+            defaultValue={decisionProblemInput.name}
+            //className={classes.textField}
+            helperText="Enter Arity"
+            onChange={handleInput}
+          />
+        </div>
+        <div>
+          <TextField
+            label="Certificate"
+            name="Certificate"
+            multiline
+            minRows={4}
+            defaultValue={decisionProblemInput.name}
+            //className={classes.textField}
+            helperText="Enter Certificate"
+            onChange={handleInput}
+          />
+        </div>
+      </div>
+        
+      
+      
+
+
+    )
+
+
 }
         
    
