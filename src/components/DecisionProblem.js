51fe552691fd32c2a3f1edab6258142c879d30cc
gracewainaina <47871572+gracewainaina@users.noreply.github.com>
2022-05-12 22:42:41 -0500

    jaz/gracew generate decision-problem

--- src/components/DecisionProblem.js
+++ src/components/DecisionProblem.js
@@ -16,17 +16,17 @@ const DecisionProblem = ({ output, setOutput }) => {
         }
     );
     
-    const handleGenerateDecisionProblem = (evt) => {
+    const handleGenerate = (evt) => {
       evt.preventDefault();
       let data = { decisionProblemInput }["decisionProblemInput"];
       const combinedDecisionProblem = "(decision-problem " + data["Name"]
         + "([" + data["Instance"] 
         + "is-a (" + data["Arity"] 
         + ")])" + data["Certificate"];
-
+      console.log(combinedDecisionProblem);
+      return combinedDecisionProblem;
     }
-     
-
+    
     const handleInput = (evt) => {
       const name = evt.target.name;
       const newValue = evt.target.value;
@@ -35,54 +35,62 @@ const DecisionProblem = ({ output, setOutput }) => {
     
     return (
       <div className="decision-problem">
-        <div className="input-name">
-          <TextField
-            label="Name"
-            name="Name"
-            multiline
-            minRows={4}
-            defaultValue={decisionProblemInput.name}
-            //className={classes.textField}
-            helperText="Enter Name"
-            onChange={handleInput}
-          />
-        </div>
-        <div className="input-instance">
-          <TextField
-            label="Instance"
-            name="Instance"
-            multiline
-            minRows={4}
-            defaultValue={decisionProblemInput.name}
-            //className={classes.textField}
-            helperText="Enter Instance"
-            onChange={handleInput}
-          />
-        </div>
-        <div className="input-arity">
-          <TextField
-            label="Arity"
-            name="Arity"
-            multiline
-            minRows={4}
-            defaultValue={decisionProblemInput.name}
-            //className={classes.textField}
-            helperText="Enter Arity"
-            onChange={handleInput}
-          />
-        </div>
-        <div className="input-certificate">
-          <TextField
-            label="Certificate"
-            name="Certificate"
-            multiline
-            minRows={4}
-            defaultValue={decisionProblemInput.name}
-            //className={classes.textField}
-            helperText="Enter Certificate"
-            onChange={handleInput}
-          />
-        </div>
+          <div className="input-name">
+            <TextField
+              label="Name"
+              name="Name"
+              multiline
+              minRows={4}
+              defaultValue={decisionProblemInput.name}
+              //className={classes.textField}
+              helperText="Enter Name"
+              onChange={handleInput}
+            />
+          </div>
+          <div className="input-instance">
+            <TextField
+              label="Instance"
+              name="Instance"
+              multiline
+              minRows={4}
+              defaultValue={decisionProblemInput.name}
+              //className={classes.textField}
+              helperText="Enter Instance"
+              onChange={handleInput}
+            />
+          </div>
+          <div className="input-arity">
+            <TextField
+              label="Arity"
+              name="Arity"
+              multiline
+              minRows={4}
+              defaultValue={decisionProblemInput.name}
+              //className={classes.textField}
+              helperText="Enter Arity"
+              onChange={handleInput}
+            />
+          </div>
+          <div className="input-certificate">
+            <TextField
+              label="Certificate"
+              name="Certificate"
+              multiline
+              minRows={4}
+              defaultValue={decisionProblemInput.name}
+              //className={classes.textField}
+              helperText="Enter Certificate"
+              onChange={handleInput}
+            />
+          </div>
+          <Button
+            type="generate"
+            variant="contained"
+            color="primary"
+            onClick={handleGenerate}
+          >
+            Generate
+          </Button>
       </div>
         
       
